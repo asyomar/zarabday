e6920ad792482398b8ed17fb3070cf0e81b8fdaa
Asyraf Omar <asyrafomar68@gmail.com>
2025-09-13 22:13:22 +0800

    fix image uploading

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (cfg, options = {}) => {
-    cfg.externals.push('sharp')
-    const { webpack } = options
-    const regex = /^sharp$/
-    cfg.plugins.push(new webpack.IgnorePlugin({
-      resourceRegExp: regex,
-    }))
-  }
-}
+  experimental: {
+    // Ensures the native module is traced/copied for serverless functions
+    serverComponentsExternalPackages: ["sharp"],
+  },
+};
 export default nextConfig;
